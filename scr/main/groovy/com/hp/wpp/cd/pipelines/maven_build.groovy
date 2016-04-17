51cc1ec174f08ff229b42649abbcd67b775d192f
vijaykookkal <vk.kookkal@gmail.com>
2016-04-17 09:06:35 +0530

    Update maven_build.groovy

--- scr/main/groovy/com/hp/wpp/cd/pipelines/maven_build.groovy
+++ scr/main/groovy/com/hp/wpp/cd/pipelines/maven_build.groovy
@@ -5,8 +5,8 @@ def call(credentialsId,url) {
     // you can call any valid step functions from your code, just like you can from Pipeline scripts
 
 
-   git credentialsId: '7a9d9c98-fec1-4a3c-82ff-53295d9d5c9b', url: 'https://github.com/WPPg2/avatar-reg.git'
-
+   //git credentialsId: '7a9d9c98-fec1-4a3c-82ff-53295d9d5c9b', url: 'https://github.com/WPPg2/avatar-reg.git'
+echo "${credentialsId}"
   // build_version = get_version()
 //   echo "Building version ${build_version}"
  //  echo "build id is ${env.BUILD_ID}"
